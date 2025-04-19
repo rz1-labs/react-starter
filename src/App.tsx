f04668320b2ff4dc79c09da95652af5f5b1e3fe6
mdrizwanfk <rizwanfardeenkhan@outlook.com>
2025-04-19 13:50:36 +0530

    feat: Tools and their versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,66 @@
+import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axiosLogo from './assets/axios.svg'
+import reactRouterLogo from './assets/react-router.svg'
 import './App.css'
+import meta from '../package.json'
+
+interface Tool {
+  name: string
+  href: string
+  version: string
+  logo?: string
+}
+
+const stack: Tool[] = [
+  {
+    name: 'vite',
+    href: 'https://vite.dev',
+    version: meta.devDependencies['vite'],
+    logo: viteLogo,
+  },
+  {
+    name: 'react',
+    href: 'https://react.dev',
+    version: meta.dependencies['react'],
+    logo: reactLogo,
+  },
+  {
+    name: 'axios',
+    href: 'https://axios-http.com/',
+    version: meta.dependencies['axios'],
+    logo: axiosLogo,
+  },
+  {
+    name: 'react-router',
+    href: 'https://reactrouter.com/',
+    version: meta.dependencies['react-router'],
+    logo: reactRouterLogo,
+  },
+]
 
 function App() {
+  const [message, setMessage] = useState('Welcome!')
+
+  const mouseHoverHandler = (tool: Tool) => {
+    setMessage(`${tool.name} v${tool.version.substring(1)}`)
+  }
+
   return (
     <>
+      <h1>{message}</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {stack.map((tool) => (
+          <a
+            href={tool.href}
+            target="_blank"
+            onMouseOver={() => mouseHoverHandler(tool)}
+          >
+            <img src={tool.logo} className="logo" alt={`${tool.name} logo`} />
+          </a>
+        ))}
       </div>
     </>
   )
